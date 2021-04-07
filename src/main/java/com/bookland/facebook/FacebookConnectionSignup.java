6aa73f415a5c94b4249a7260a23efc2662278d6c
timy116 <timy117@gmail.com>
2021-04-07 12:43:23 +0800

    完成 Facebook 登入功能
    1. 新增 FB 登入相關檔案
    2. 實現後端邏輯

---
+++ src/main/java/com/bookland/facebook/FacebookConnectionSignup.java
@@ -0,0 +1,32 @@
+package com.bookland.facebook;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+
+import com.bookland.entity.User;
+import com.bookland.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FacebookConnectionSignup implements ConnectionSignUp {
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    // 若該使用者是第一次從 facebook 登入，則綁訂一個新的使用者
+    public String execute(Connection<?> connection) {
+        final User user = new User();
+        user.setUserName(connection.getDisplayName());
+        user.setPassword(encoder.encode(randomAlphanumeric(32)));
+        user.setEmail("!");
+        userService.create(user);
+        return user.getUserName();
+    }
+}
